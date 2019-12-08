42efa710d3fe837c2eb984be41436d7eaef42ed7
Nicholas Sizer <senseibaka@senseibaka.com>
2019-12-08 15:30:05 +1030

    Cleanup 2019 day05

--- 2019/day05/src/main.rs
+++ 2019/day05/src/main.rs
@@ -3,14 +3,23 @@ use intcode::intcode::*;
 
 fn main() {
     let program_spec = first_line(file_to_vec("input.txt".to_string()).unwrap());
-    let (_, output) = run_emulator(program_spec.to_string(), "1".to_string(), false);
-    println!("PART 1 OUTPUT: {}", output);
-    let (_, output) = run_emulator(program_spec.to_string(), "5".to_string(), false);
-    println!("PART 2 OUTPUT: {}", output);
+    println!(
+        "PART 1 OUTPUT: {}",
+        run_emulator(program_spec.to_string(), "1".to_string(), false)
+    );
+    println!(
+        "PART 2 OUTPUT: {}",
+        run_emulator(program_spec.to_string(), "5".to_string(), false)
+    );
 }
 
-fn run_emulator(program_spec: String, input_spec: String, debug: bool) -> (String, String) {
+fn run_emulator(program_spec: String, input_spec: String, debug: bool) -> i32 {
     let mut emulator = prepare_emulator(program_spec, input_spec, debug);
-    emulator.run_program();
-    return deconstruct_output(emulator);
+    loop {
+        match emulator.run_program() {
+            RunSignal::Halt => break,
+            _ => continue,
+        }
+    }
+    return emulator.outputs.pop().unwrap();
 }
