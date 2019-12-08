ce4624502f9e9dc133d237aab3ce89491217d182
Nicholas Sizer <senseibaka@senseibaka.com>
2019-12-08 15:26:41 +1030

    Cleanup 2019 day04

--- 2019/day04/src/main.rs
+++ 2019/day04/src/main.rs
@@ -3,24 +3,25 @@ use std::iter::Iterator;
 fn main() {
     let range1 = 284639..=748759;
     let valid1 = range1
-        .filter(|x| has_same_adjacent(*x))
-        .filter(|x| never_deacreases(*x))
-        .fold(0, |acc, x| acc + 1);
+        .map(|x| x.to_string())
+        .filter(|x| never_deacreases(x))
+        .filter(|x| has_same_adjacent(x))
+        .count();
     println!("part 1: {}", valid1);
-    
+
     let range2 = 284639..=748759;
     let valid2 = range2
-        //.filter(|x| has_same_adjacent(*x))
-        .filter(|x| contains_an_exact_pair(*x))
-        .filter(|x| never_deacreases(*x))
-        .fold(0, |acc, x| acc + 1);
+        .map(|x| x.to_string())
+        .filter(|x| never_deacreases(x))
+        .filter(|x| contains_an_exact_pair(x))
+        .count();
     println!("part 2: {}", valid2);
 }
 
-fn has_same_adjacent(candidate: i32) -> bool {
-    let chars: Vec<char> = candidate.to_string().chars().collect();
+fn has_same_adjacent(candidate: &String) -> bool {
+    let chars: Vec<char> = candidate.chars().collect();
     for i in 1..chars.len() {
-        let prev = chars[i-1];
+        let prev = chars[i - 1];
         let cur = chars[i];
         if prev == cur {
             return true;
@@ -30,23 +31,22 @@ fn has_same_adjacent(candidate: i32) -> bool {
     return false;
 }
 
-fn contains_an_exact_pair (candidate: i32) -> bool {
-    let s: String = candidate.to_string();
-    let chars: Vec<char> = s.chars().collect();
+fn contains_an_exact_pair(candidate: &String) -> bool {
+    let chars: Vec<char> = candidate.chars().collect();
     for c in chars {
-        if s.matches(c).count() == 2 {
+        if candidate.matches(c).count() == 2 {
             return true;
         }
     }
     return false;
 }
 
-fn never_deacreases(candidate: i32) -> bool {
-    let chars: Vec<char> = candidate.to_string().chars().collect();
+fn never_deacreases(candidate: &String) -> bool {
+    let chars: Vec<char> = candidate.chars().collect();
     for i in 1..chars.len() {
-        let prev = chars[i-1];
+        let prev = chars[i - 1];
         let cur = chars[i];
-        
+
         if prev > cur {
             return false;
         }
@@ -59,25 +59,22 @@ mod tests {
     use super::*;
     #[test]
     fn has_same_adjacent_works() {
-        assert_eq!(true, has_same_adjacent(111111));
-        assert_eq!(true, has_same_adjacent(223450));
-
-        assert_eq!(false, has_same_adjacent(123789));
+        assert_eq!(true, has_same_adjacent(&"111111".to_string()));
+        assert_eq!(true, has_same_adjacent(&"223450".to_string()));
+        assert_eq!(false, has_same_adjacent(&"123789".to_string()));
     }
-    
+
     #[test]
     fn never_deacreases_works() {
-        assert_eq!(true, never_deacreases(111111));
-        assert_eq!(true, never_deacreases(123789));
-
-        assert_eq!(false, never_deacreases(223450));
+        assert_eq!(true, never_deacreases(&"111111".to_string()));
+        assert_eq!(true, never_deacreases(&"123789".to_string()));
+        assert_eq!(false, never_deacreases(&"223450".to_string()));
     }
 
     #[test]
     fn contains_an_exact_pair_works() {
-        assert_eq!(true, contains_an_exact_pair(112233));
-        assert_eq!(true, contains_an_exact_pair(111122));
-
-        assert_eq!(false, contains_an_exact_pair(123444));
+        assert_eq!(true, contains_an_exact_pair(&"112233".to_string()));
+        assert_eq!(true, contains_an_exact_pair(&"111122".to_string()));
+        assert_eq!(false, contains_an_exact_pair(&"123444".to_string()));
     }
 }
