11d4d73609ccd2163c5dbaa671f95e9b00b88e98
Nicholas Sizer <senseibaka@senseibaka.com>
2019-12-09 20:19:10 +1030

    Add 2019 day09

---
+++ 2019/day09/src/main.rs
@@ -0,0 +1,25 @@
+use common::*;
+use intcode::intcode::*;
+
+fn main() {
+    let program_spec = first_line(file_to_vec("input.txt".to_string()).unwrap());
+    println!(
+        "PART 1 OUTPUT: {}",
+        run_emulator(program_spec.to_string(), "1".to_string(), false)
+    );
+    println!(
+        "PART 2 OUTPUT: {}",
+        run_emulator(program_spec.to_string(), "2".to_string(), false)
+    );
+}
+
+fn run_emulator(program_spec: String, input_spec: String, debug: bool) -> i64 {
+    let mut emulator = prepare_emulator(program_spec, input_spec, debug);
+    loop {
+        match emulator.run_program() {
+            RunSignal::Halt => break,
+            _ => continue,
+        }
+    }
+    emulator.outputs.pop().unwrap()
+}
