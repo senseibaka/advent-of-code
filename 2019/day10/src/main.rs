f0cf5427e06f90f72fb84ad15fe17465407c04a7
Nicholas Sizer <senseibaka@senseibaka.com>
2019-12-10 23:56:35 +1030

    Add 2019 day 10 part 2 - partial
    Solution found manually from stdout, need to sort the results in-code and print a result.

--- 2019/day10/src/main.rs
+++ 2019/day10/src/main.rs
@@ -1,17 +1,35 @@
 use common::*;
 use std::f64;
 use std::cmp::Eq;
+//use std::cmp::Ordering;
 use std::hash::Hash;
 use std::hash::Hasher;
 use std::collections::HashSet;
+use std::collections::HashMap;
+//use std::collections::BTreeMap;
 
 fn main() {
     let lines = file_to_vec("input.txt".to_string()).unwrap();
-    let (station, p1sol) = part_1_solution(&lines);
-    println!("Place the station at {:?}, it can see {} asteroids", station, p1sol);
+    let (station, p1sol, visible) = part_1_solution(&lines);
+    println!("Place the station at {:?}, it can see {} asteroids, which are:", station, p1sol);
+    //let mut ordered_visible: BTreeMap<Double, (i32, i32)> = BTreeMap::new();
+    for asteroid in visible {
+        let angle = (asteroid.0).0;
+        //let mut adjusted = angle + f64::consts::FRAC_PI_2;
+        let mut adjusted = f64::consts::FRAC_PI_2 - angle;
+        if adjusted < 0.0 {
+            adjusted = adjusted + f64::consts::PI + f64::consts::PI;
+        }
+        //let adjusted = angle;
+        //println!("{} => {:?} ", (asteroid.0).0, asteroid.1);
+        println!("{} => {:?}", adjusted, asteroid.1);
+    }
+    //cargo run > output.txt
+    //sort the lines in your text editor, 200th line is the point :)
+    //TODO: sort the stuff in code and print the result
 }
 
-fn part_1_solution(lines: &Vec<String>) -> ((i32, i32), usize) {
+fn part_1_solution(lines: &Vec<String>) -> ((i32, i32), usize, HashMap<Double, (i32, i32)>) {
 let mut x = 0;
     let mut y = 0;
     let mut asteroids: HashSet<(i32, i32)> = HashSet::new();
@@ -28,30 +46,46 @@ let mut x = 0;
     }
 
     let mut best_count: usize = 0;
+    let mut best_visible: HashMap<Double, (i32, i32)> = HashMap::new();
     let mut best: (i32, i32) = (0,0);
     for asteroid in &asteroids {
-        let mut angles: HashSet<Double> = HashSet::new();
+        let mut angles: HashMap<Double, (i32, i32)> = HashMap::new();
         for other_asteroid in &asteroids {
             if asteroid == other_asteroid {
                 continue;
             }
-            let a = ((other_asteroid.1 - asteroid.1) as f64).atan2(
+            let a = ((asteroid.1 - other_asteroid.1) as f64).atan2(
                 (other_asteroid.0 - asteroid.0) as f64
             );
-            angles.insert(Double(a));
+            //check which is closest to the asteroid, and store that
+            let angle = Double(a);
+            match angles.get(&angle) {
+                Some(existing) => {
+                    let existing_distance = (existing.0 - asteroid.0).abs() + (existing.1 - asteroid.1).abs();
+                    let candidate_distance = (other_asteroid.0 - asteroid.0).abs() + (other_asteroid.1 - asteroid.1).abs();
+                    //println!("{:?} vs {:?}", existing, other_asteroid);
+                    if candidate_distance < existing_distance {
+                        angles.insert(Double(a), (other_asteroid.0, other_asteroid.1));
+                    }
+                },
+                None => {
+                    angles.insert(Double(a), (other_asteroid.0, other_asteroid.1));
+                }
+            };
         }
 
         let count = angles.len();
         if count > best_count {
             best_count = count;
+            best_visible = angles.clone();
             best = (asteroid.0, asteroid.1);
         }
     }
 
-    (best, best_count)
+    (best, best_count, best_visible)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Double(f64);
 
 impl Double {
@@ -73,3 +107,9 @@ impl Hash for Double {
         self.canonicalize().hash(state);
     }
 }
+
+/* impl Ord for Double {
+    fn cmp(&self, other: &Self) -> Ordering {
+
+    }
+} */
